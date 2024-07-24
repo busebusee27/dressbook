8e11091b747ce583e97e1708b40c07f1ad547f11
Haani <haani@mit.edu>
2024-07-24 08:07:04 +0500

    feat: added page transitions

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:dressbook/services/app_state.dart';
 import 'package:dressbook/views/dress_book.dart';
 import 'package:dressbook/views/sign_in.dart';
 import 'package:dressbook/views/splash_screen.dart';
-import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
@@ -17,22 +16,26 @@ void main() {
   ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'DressBook',
       theme: appTheme,
       home: const SplashScreen(),
+      defaultTransition: Transition.native,
+      transitionDuration: const Duration(seconds: 1),
       getPages: [
         GetPage(name: '/home', page: () => const DressBookScreen()),
         GetPage(name: '/splash', page: () => const SplashScreen()),
-        GetPage(
-          name: '/sign-in',
-          page: () => const LogInScreen(),
-        ),
+        GetPage(name: '/sign-in', page: () => const LogInScreen()),
       ],
     );
   }
